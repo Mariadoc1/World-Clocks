4ada85f93fec33b75c4e23995772e35a8daea543
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2023-12-15 12:38:34 +0000

    Added Tokyo as a timezone on the homepage

--- javascript/index.js
+++ javascript/index.js
@@ -24,6 +24,19 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#city-tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
