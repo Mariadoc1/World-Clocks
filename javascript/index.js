40df4172645dd27b0a0a8e773bab623a6c923282
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2023-12-15 11:48:42 +0000

    Moved timezones in to one function called updateTime
    All the time and dates are now live and they update every second

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+function updateTime() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#city-los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Paris
+  let parisElement = document.querySelector("#city-paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/France");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
